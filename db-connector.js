45a72d611b68079a5575466eefc725c655608913
Melisa Im <melisaevelynim@gmail.com>
2021-05-19 12:57:05 -0700

    Update mysql config

--- db-connector.js
+++ db-connector.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 var pool = mysql.createPool({
     connectionLimit : 1000,
     connectTimeout: 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : process.env.USERNAME,
     password        : process.env.PW,
