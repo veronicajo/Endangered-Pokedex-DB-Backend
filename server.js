c4c87d23a5c732675a69d9aff834fe95214fedea
Melisa Im <melisaevelynim@gmail.com>
2021-05-19 21:18:35 -0700

    Add SELECT events for all tables

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get('/get/numberLeft', (req, res) => {
+    const tableName = req.params.table;
+        const sqlSelect = `SELECT numberLeftId, CASE WHEN inCaptivity = -1 THEN 'UNKNOWN' ELSE inCaptivity END as inCaptivity, inWild, decade, conservationStatus, (SELECT es.commonName) as animal, es.animalId FROM numberLeft nl INNER JOIN endangeredSpecies es ON es.animalId = nl.animalId`;
+        db.pool.query(sqlSelect, (err, result) => {
+            console.log("QUERY ERROR ", err);
+            console.log(result);
+            res.send(JSON.stringify(result));
+        });
+});
+
 app.get('/get/:table', (req, res) => {
     const tableName = req.params.table;
     if (tableName) {
@@ -25,6 +35,34 @@ app.get('/get/:table', (req, res) => {
         res.send("Incorrect table requested");
     }
 });
+
+app.get('/get/endangeredHabitats/nativeHabitats/habitatId', (req, res) => {
+        const sqlSelect = `SELECT es.commonName as animal, es.animalId, h.habitatId, h.nativeHabitatCoordinates as location FROM
+        (
+            endangeredHabitats
+            INNER JOIN endangeredSpecies es ON endangeredHabitats.animalId = es.animalId
+            INNER JOIN nativeHabitats h ON endangeredHabitats.habitatId = h.habitatId
+        );`;
+        db.pool.query(sqlSelect, (err, result) => {
+            console.log("QUERY ERROR ", err);
+            console.log(result);
+            res.send(JSON.stringify(result));
+        });
+})
+
+app.get('/get/endangeredNonprofits/nonprofits/nonprofitId', (req, res) => {
+        const sqlSelect = `SELECT es.commonName as animal, np.nonprofitName as nonprofit, es.animalId, np.nonprofitId FROM
+        (
+            endangeredNonprofits
+            INNER JOIN endangeredSpecies es ON endangeredNonprofits.animalId = es.animalId
+            INNER JOIN nonprofits np ON endangeredNonprofits.nonprofitId = np.nonprofitId
+        );`;
+        db.pool.query(sqlSelect, (err, result) => {
+            console.log("QUERY ERROR ", err);
+            console.log(result);
+            res.send(JSON.stringify(result));
+        });
+})
 
 /*
     LISTENER
